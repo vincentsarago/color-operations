0c4be275505f19295ff34b187d77f1aac2c1298c
Kyle Barron <kyle@developmentseed.org>
2024-06-26 12:14:53 -0400

    pre-commit lint

--- python/color_operations/colorspace.py
+++ python/color_operations/colorspace.py
@@ -1,4 +1,4 @@
 """Maintained for backwards compatibility"""
 
-from ._rust import convert, convert_arr, saturate_rgb  # noqa
 from ._enums import ColorSpace  # noqa
+from ._rust import convert, convert_arr, saturate_rgb  # noqa
