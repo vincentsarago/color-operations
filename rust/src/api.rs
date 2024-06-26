a570808162331408fba3b6d5c721ef49de4b8ab6
Kyle Barron <kyle@developmentseed.org>
2024-06-26 10:26:00 -0400

    Bump to pyo3 0.21

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -10,15 +10,15 @@ fn saturate_rgb<'py>(
     py: Python<'py>,
     arr: PyReadonlyArray3<f64>,
     satmult: f64,
-) -> &'py PyArray3<f64> {
+) -> Bound<'py, PyArray3<f64>> {
     let arr = arr.as_array();
     let out = crate::saturation::saturate_rgb(arr, satmult);
-    out.into_pyarray(py)
+    out.into_pyarray_bound(py)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-pub fn color_operations(_py: Python, m: &PyModule) -> PyResult<()> {
+pub fn color_operations(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(saturate_rgb, m)?)?;
     Ok(())
 }
